8c96d877541f0c81ccc20fc348e413eb38eae293
unknown <navasarroyojoseantonio@gmail.com>
2026-06-30 14:20:15 +0200

    aparicion smooth de posts

--- app.js
+++ app.js
@@ -10,3 +10,19 @@ botonOscuro.addEventListener("click", function() {
         botonOscuro.textContent="🌙";
     }
 });
+
+//logica aparicion de pizzas
+const observador=new IntersectionObserver((entradas) => {
+    entradas.forEach(entrada => {
+        if(entrada.isIntersecting){
+            entrada.target.classList.remove("oculto");
+            entrada.target.classList.add("mostrar");
+        }
+    });
+});
+
+const pizzas=document.querySelectorAll(".post");
+
+pizzas.forEach(pizza => {
+    observador.observe(pizza);   
+});
